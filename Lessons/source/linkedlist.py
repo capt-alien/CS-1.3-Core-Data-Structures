722584f4dabdd09602d89407f3198d2fb9c49451
capt-alien <eric.botcher@presidio.edu>
2019-04-15 14:48:09 -0700

    get at index

--- Lessons/source/linkedlist.py
+++ Lessons/source/linkedlist.py
@@ -78,7 +78,10 @@ class LinkedList(object):
         # Check if the given index is out of range and if so raise an error
         if not (0 <= index < self.size):
             raise ValueError('List index out of range: {}'.format(index))
-        # TODO: Find the node at the given index and return its data
+        current_node = self.head
+        for count in range(0,index):
+            current_node = current_node.next
+        return current_node.data
 
     def insert_at_index(self, index, item):
         """Insert the given item at the given index in this linked list, or
